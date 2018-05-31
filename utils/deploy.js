118d813f2f95be0cc28286ffb2783511bb981b3c
weirdpattern <patricio@weirdpattern.com>
2018-05-30 20:58:25 -0500

    Update deploy script... sorry testing...

--- utils/deploy.js
+++ utils/deploy.js
@@ -1,10 +1,16 @@
 const push = require("git-push");
 
 const remote = {
+  name: "live",
   url: "git@github.com:weirdpattern/weirdpattern.com",
   branch: "live"
 };
 
-push("./public", remote, function() {
-  console.log("Site deployed to live branch");
+push("./public", remote, function(err) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.info("Site deployed to live branch");
 });
